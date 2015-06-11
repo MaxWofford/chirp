16299db0916ffc756d3b5d8f3dbe3363b11e4afd
Max Wofford <max@hackedu.us>
2015-06-11 11:44:16 -0700

    firebase keeps tweet data

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
+var fb = new Firebase('https://chirp-example.firebaseio.com/');
+
 var submitTweet = function() {
-  var tweetInput = document.getElementById('tweet-input');
+  var tweetMessage = document.getElementById('tweet-input').value;
 
   // 1. Check if there is a tweet available
-  if (tweetInput.value === '')
+  if (tweetMessage === '')
     return
-  // 2. Add the tweet to the list of tweets
+
+  // 2. Push the tweet to Firebase
+  var userImage = document.getElementById('user-icon');
+  if (userImage.value === '')
+    icon = '//placehold.it/100/100';
+  else
+    icon = userImage.value;
+  fb.push({message: tweetMessage, icon: icon});
+
+  // 3. Clear the tweet input and reset
+  tweetMessage = '';
+}
+
+var updateTweetList = function(snapshot) {
+
   var newTweet = document.createElement('p');
   var tweets = document.getElementById('tweets');
-  var userImage = document.getElementById('user-icon');
-  if (userImage.value != '') {
+  if (snapshot.icon != '') {
     var userIcon = document.createElement('img');
-    userIcon.src = userImage.value;
+    userIcon.src = snapshot.icon;
     newTweet.appendChild(userIcon);
   }
-  newTweet.innerHTML += tweetInput.value;
+  newTweet.innerHTML += snapshot.message;
   tweets.appendChild(newTweet);
-   
-  // 3. Clear the tweet input and reset
-  tweetInput.value = '';
 }
 
 var submitButton = document.getElementById('submit');
 
 submitButton.onclick = submitTweet;
+
+fb.on('child_added', function(snapshot) {
+  updateTweetList(snapshot.val());
+}, function (errorObject) {
+  console.log('The read failed: ' + errorObject.code);
+});
